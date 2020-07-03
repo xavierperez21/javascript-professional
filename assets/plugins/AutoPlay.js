da34bfd525215140d1c39272a24fc165f3149021
Xavierperez <xavierpm1221@gmail.com>
2020-07-03 11:21:27 -0500

    Adding folder of exercises

--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -2,8 +2,8 @@ function AutoPlay() {
 }
 
 AutoPlay.prototype.run = function (player) {
-    // player.mute();
-    // player.play();
+    player.mute();
+    player.play();
 }
 
 export default AutoPlay;
