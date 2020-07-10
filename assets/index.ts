e07d1b26ea19d5e1f55616a6636558deb18656ca
Xavierperez <xavierpm1221@gmail.com>
2020-07-10 16:34:14 -0500

    Adding last plugin: Ads and finishing project

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,15 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video');  // querySelector takes as an argument a selector (it could be a label like 'video' or a class .video or an id #video)
 const player = new MediaPlayer({ 
     el: video, 
     plugins: [
         new AutoPlay(),
-        new AutoPause()
+        new AutoPause(),
+        new Ads(),
     ]
 });
 
